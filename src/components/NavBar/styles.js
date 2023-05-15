e63386042b399e3ea4b923d2b533162b4986087b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-15 23:38:05 +0400

    add sidebar styles

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@mui/styles";
 
+const drawerWidth = 240;
+
 export default makeStyles((theme) => ({
     toolbar: {
         height: '80px',
@@ -17,4 +19,20 @@ export default makeStyles((theme) => ({
             display: "none",
         },
     },
+    drawer: {
+        [theme.breakpoints.up("sm")]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        }, 
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+linkButton : {
+    '&:hover': {
+        color: 'white !important',
+        textdecoration: 'none !important',
+    }
+}
+
 }));
