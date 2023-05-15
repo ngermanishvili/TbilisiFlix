df1295f3aff89c3c36cfe7dd2573078458a1a6f5
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-15 22:35:43 +0400

    edit navbar styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+  Icon,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
 
 const NavBar = () => {
-  console.log("NavBar");
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
-  return <div>NavBar</div>;
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton
+            edge="start"
+            color="inherit"
+            style={{ outline: "none" }}
+            onClick={() => {}}
+            className={classes.menu}
+          >
+            <Menu />
+          </IconButton>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
