8a2b30a631012d9301c35561372e135826109c92
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-15 23:06:01 +0400

    navbar markup done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,10 +17,13 @@ import {
 } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
+import { useTheme } from "@mui/material/styles";
 
 const NavBar = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(max-width: 600px)");
+  const theme = useTheme();
+  const isAuthenticated = true;
 
   return (
     <AppBar position="fixed">
@@ -36,6 +39,34 @@ const NavBar = () => {
             <Menu />
           </IconButton>
         )}
+        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+          {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        {!isMobile && "Search ..."}
+        <div>
+          {!isAuthenticated ? (
+            <Button color="inherit" onClick={() => {}}>
+              Login &nbsp; <AccountCircle />
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              component={Link}
+              to={`$profile/:id`}
+              className={classes.linkButton}
+              onClick={() => {}}
+            >
+              {!isMobile && <>My Movies &nbsp; </>}
+
+              <Avatar
+                style={{ width: 30, height: 30 }}
+                alt="Profile"
+                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+              />
+            </Button>
+          )}
+        </div>
+        {isMobile && "Search ..."}
       </Toolbar>
     </AppBar>
   );
