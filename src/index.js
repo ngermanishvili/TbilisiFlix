15daa94a922f0e9a18a0d22fd73745ef9809757f
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-15 22:36:08 +0400

    modified main index for add themeprovider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+const theme = createTheme({});
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ThemeProvider theme={theme}>
+            <App />
+    </ThemeProvider>,
 )
