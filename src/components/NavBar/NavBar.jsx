3239c7d34a2d4c23088ddb65e019abde1f692fef
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-15 23:38:31 +0400

    add sidebar inside navbar and minor functionally

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   IconButton,
@@ -18,57 +18,85 @@ import {
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import { useTheme } from "@mui/material/styles";
+import Sidebar from "../Sidebar/Sidebar";
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery("(max-width: 600px)");
   const theme = useTheme();
   const isAuthenticated = true;
 
   return (
-    <AppBar position="fixed">
-      <Toolbar className={classes.toolbar}>
-        {isMobile && (
-          <IconButton
-            edge="start"
-            color="inherit"
-            style={{ outline: "none" }}
-            onClick={() => {}}
-            className={classes.menu}
-          >
-            <Menu />
-          </IconButton>
-        )}
-        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
-          {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-        {!isMobile && "Search ..."}
-        <div>
-          {!isAuthenticated ? (
-            <Button color="inherit" onClick={() => {}}>
-              Login &nbsp; <AccountCircle />
-            </Button>
-          ) : (
-            <Button
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              edge="start"
               color="inherit"
-              component={Link}
-              to={`$profile/:id`}
-              className={classes.linkButton}
+              style={{ outline: "none" }}
               onClick={() => {}}
+              className={classes.menu}
             >
-              {!isMobile && <>My Movies &nbsp; </>}
-
-              <Avatar
-                style={{ width: 30, height: 30 }}
-                alt="Profile"
-                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-              />
-            </Button>
+              <Menu />
+            </IconButton>
           )}
-        </div>
-        {isMobile && "Search ..."}
-      </Toolbar>
-    </AppBar>
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && "Search ..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`$profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp; </>}
+
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search ..."}
+        </Toolbar>
+      </AppBar>
+      {/* SIDEBAR */}
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={mobileOpen}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keepMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+            <Drawer
+              classes={{ paper: classes.drawerPaper }}
+              variant="permanent"
+              open
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
   );
 };
 
