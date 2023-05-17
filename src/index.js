1aa4ab0ae470ed23af4222815c4e31134239ff0b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-17 16:54:26 +0400

    Issue: cant get reducers and have app error

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
-const theme = createTheme({});
-
+const theme = createTheme({
+  typography: {
+    fontSize: 16,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
     <ThemeProvider theme={theme}>
-            <App />
-    </ThemeProvider>,
-)
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
+);
