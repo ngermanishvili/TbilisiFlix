1079da454f4730cf8cdcac6decd24867546c7618
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-17 16:53:55 +0400

    add tmdb logic and make hook for entire app

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,1 +1,24 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_API_KEY;
+const page = 1;
+
+export const tmdbapi = createApi({
+    reducerPath: 'tmdbapi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://api.themoviedb.org/3/',
+    }),
+    endpoints: (builder) => ({
+        //* Get movie by type
+        getmovies: builder.query({
+            query: () => {
+                return {
+                    url: `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+                }
+            }
+        })
+    })
+})
+
+export const { useGetmoviesQuery } = tmdbapi;
