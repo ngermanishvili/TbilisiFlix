6007ee01da4533443d7ce4ba56ca72ee2c2ad66e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-18 18:57:36 +0400

    modified store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { tmdbApi } from '../services/TMDB'
 
 export default configureStore({
-    reducers: {
-        // Here we will be adding reducers
-    }
-})
+    reducer: {
+        [tmdbApi.reducerPath]: tmdbApi.reducer,
+    },
+});
