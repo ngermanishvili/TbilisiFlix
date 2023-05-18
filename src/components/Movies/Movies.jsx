c4d77fefdaea8c3cce44ba00b1b04ae4a56f219b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-18 18:57:14 +0400

    minor changes

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+
+import { useSelector } from "react-redux";
+import { useGetmoviesQuery } from "../../services/TMDB";
 
 const Movies = () => {
-  console.log("Movies");
-
+  const { data } = useGetmoviesQuery();
+  console.log(data);
   return <div>Movies</div>;
 };
 
