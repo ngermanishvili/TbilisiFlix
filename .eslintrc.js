98636eb861a398725e41624510ead09308da057b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-19 14:40:42 +0400

    configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,23 +7,40 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
   ],
-  overrides: [
-  ],
   parserOptions: {
+    parser: 'babel-eslint',
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   plugins: [
     'react',
   ],
   rules: {
+    'spaced-comment': [
+      'error',
+      'never',
+      {
+        block: {
+          balanced: true,
+        },
+      },
+    ],
     'import/no-cycle': 0,
     'no-console': 0,
     'react/prop-types': 0,
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
-    'max-len': [2, 250],
+    'max-len': 0,
     'object-curly-newline': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-one-expression-per-line': 0,
