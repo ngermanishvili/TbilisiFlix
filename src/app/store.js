cd798af4c78726dee01d867187ad3bce3ca2afe2
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-19 14:40:21 +0400

    fix: after middleware adding the code is working well and add reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { tmdbApi } from '../services/TMDB'
+import { tmdbApi } from '../services/TMDB';
 
-export default configureStore({
-    reducer: {
-        [tmdbApi.reducerPath]: tmdbApi.reducer,
-    },
+const store = configureStore({
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tmdbApi.middleware),
 });
+
+export default store;
