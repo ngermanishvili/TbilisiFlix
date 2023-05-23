e6d63f932b8ca930acd835a2f4db8b154b859db1
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:23:27 +0400

    add mui styles into stylesjs

---
+++ src/components/MovieList/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+
+const drawerWidth = 240;
+
+export default makeStyles((theme) => ({
+moviesContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    overflow: 'auto',
+    [theme.breakpoints.down('sm')]: {
+        justifyContent: 'center',
+    }
+}
+
+}));
