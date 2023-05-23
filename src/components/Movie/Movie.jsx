af5ca5bf75711b5984017ad2dda46191b81325e4
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:48:47 +0400

    add Movies tittle inside typography

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -8,7 +8,7 @@ const Movie = ({ movie, i }) => {
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5"> 
+      <Typography className={classes.title} variant="h5">
         {movie.title}
       </Typography>
     </Grid>
