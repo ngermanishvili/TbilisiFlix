248acb6011a13d63d80220d14edf788fd835773e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:17:02 +0400

    add movielist inside indexjs

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Movies } from './Movies/Movies';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
 export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as MovieList } from './MovieList/MovieList'
