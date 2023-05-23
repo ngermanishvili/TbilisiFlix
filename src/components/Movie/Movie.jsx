f86be49b1085d00b04b30f6cb34ee27d2044a8f5
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:48:20 +0400

    import tyles and add tittle inside movie

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Typography, Grid, Grow, Tooltip, Rating } from "@mui/material";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
 
-const Movie = () => {
+const Movie = ({ movie, i }) => {
+  const classes = useStyles();
+
   return (
-    <div>Movie</div>
-  )
-}
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Typography className={classes.title} variant="h5"> 
+        {movie.title}
+      </Typography>
+    </Grid>
+  );
+};
 
-export default Movie
+export default Movie;
