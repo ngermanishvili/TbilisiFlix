1a0ecd719383b4afa30eb35917327d713eb5814d
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:49:39 +0400

    add data error and isfetching if checking operator inside movies component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -10,7 +10,30 @@ import { useSelector } from "react-redux";
 import { useGetmoviesQuery } from "../../services/TMDB";
 
 const Movies = () => {
-  const { data } = useGetmoviesQuery();
+  const { data, error, isFetching } = useGetmoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that found
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "Something went wrong";
+
   console.log(data);
   return (
     <div>
