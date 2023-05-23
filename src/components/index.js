611a7289008c82579227bd75d0b472bbbb349293
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:47:41 +0400

    update index js and add movie export default as

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
 export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as MovieList } from './MovieList/MovieList'
+export { default as Movie } from './Movie/Movie'
