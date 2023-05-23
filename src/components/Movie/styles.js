40881e7fa4a6bf12112155ee7f1b8bd999d789e3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 19:03:21 +0400

    update movie styles and done mui

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -1,20 +1,37 @@
-import { makeStyles } from "@mui/styles";
-
-const drawerWidth = 240;
+import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
-    movie: {
-        padding: '10px'
+  movie: {
+    padding: '10px',
+  },
+  title: {
+    color: theme.palette.text.primary,
+    textOverflow: 'ellipsis',
+    width: '230px',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    marginTop: '10px',
+    marginBottom: 0,
+    textAlign: 'center',
+  },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
     },
-    title: {
-        color: theme.palette.text.primary,
-        textOverflow: 'ellipsis',
-        width: '230px',
-        whiteSpace: 'nowrap',
-        overflow: "hidden",
-        marginTop: '10px',
-        marginBottom: 0,
-        textAlign: 'center',
-    }
-
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
 }));
