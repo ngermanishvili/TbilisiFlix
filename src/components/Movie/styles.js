9ef8da746d13671bb3e3c001ab58341f98d6e02e
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-23 17:50:42 +0400

    add movie styles inside mui styles folder

---
+++ src/components/Movie/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from "@mui/styles";
+
+const drawerWidth = 240;
+
+export default makeStyles((theme) => ({
+    movie: {
+        padding: '10px'
+    },
+    title: {
+        color: theme.palette.text.primary,
+        textOverflow: 'ellipsis',
+        width: '230px',
+        whiteSpace: 'nowrap',
+        overflow: "hidden",
+        marginTop: '10px',
+        marginBottom: 0,
+        textAlign: 'center',
+    }
+
+}));
