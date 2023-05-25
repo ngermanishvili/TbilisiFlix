6001b5b58ba3f49e35b011a65b3432f1c01585a3
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-25 18:02:55 +0400

    add currentgenre reducer inside store file

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from '../services/TMDB';
+import genreOrCategoryReducer from '../features/CurrentGenreOrCategory'
 
 const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
+    CurrentGenreOrCategory: genreOrCategoryReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tmdbApi.middleware),
