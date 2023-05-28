67f24a850b06eb2d1a37f77e32227ca76a661762
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-28 20:01:46 +0400

    Add TMDB API integration and movie fetching functionality

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -8,14 +8,28 @@ export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
     endpoints: (builder) => ({
-//* Get Genres
+        //* Get Genres
         getgenres: builder.query({
             query: () => `/genre/movie/list?api_key=${tmdbApiKey}`,
         }),
 
-        //* Get movie by type
+        //* Get movie by [Type]
         getmovies: builder.query({
-            query: () => {
+            query: ({ genreIdOrCategoryName, page }) => {
+
+                //*Get movies by Categoeries
+                if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+                    return {
+                        url: `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`,
+                    }
+                }
+                //*Get movies by Genres
+                if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+                    return {
+                        url: `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`,
+                    }
+                }
+
                 return {
                     url: `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
                 }
