24202ff879b2d167e6100f0340da3059be99d44b
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-29 22:00:40 +0400

    add if check also for searchquery inside tmdb

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -2,7 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
@@ -15,7 +14,12 @@ export const tmdbApi = createApi({
 
         //* Get movie by [Type]
         getmovies: builder.query({
-            query: ({ genreIdOrCategoryName, page }) => {
+            query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+                //* Get movies by Search
+
+                if(searchQuery) {
+                    return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`
+                }
 
                 //*Get movies by Categoeries
                 if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
