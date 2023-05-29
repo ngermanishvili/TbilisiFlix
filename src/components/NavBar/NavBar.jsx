ae176109619bc477447270fd6a0b41869ef34eea
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-29 20:54:32 +0400

    Import Search component inside Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ import {
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import { useTheme } from "@mui/material/styles";
-import Sidebar from "../Sidebar/Sidebar";
+import { Sidebar, Search } from "../../components";
 
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -45,7 +45,7 @@ const NavBar = () => {
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
             {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
-          {!isMobile && "ძებნა ..."}
+          {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
               <Button color="inherit" onClick={() => {}}>
@@ -69,7 +69,7 @@ const NavBar = () => {
               </Button>
             )}
           </div>
-          {isMobile && "ძებნა ..."}
+          {isMobile && <Search />}
         </Toolbar>
       </AppBar>
       {/* SIDEBAR */}
