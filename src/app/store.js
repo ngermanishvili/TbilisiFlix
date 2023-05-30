8e25584d902903b1f1b81c64132b8a55d4696c7c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-30 19:33:35 +0400

    add user reducer inside store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from '../services/TMDB';
-import genreOrCategoryReducer from '../features/CurrentGenreOrCategory'
+import genreOrCategoryReducer from '../features/CurrentGenreOrCategory';
+import userReducer from '../features/auth';
 
 const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
     CurrentGenreOrCategory: genreOrCategoryReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tmdbApi.middleware),
