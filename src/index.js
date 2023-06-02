ffadbdd11816b51e08de4667814d11f9b0450397
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-02 22:03:03 +0400

    imported global css 💅

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import './index.css'
 
 const theme = createTheme({
   typography: {
