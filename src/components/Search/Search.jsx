9917da4419cd20e3bbb9425cf1f861201e241391
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-02 22:04:26 +0400

    chore: made minor changes or improvements that do not affect functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,12 +12,15 @@ const Search = () => {
   const [query, setQuery] = useState("");
   const classes = useStyles();
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       dispatch(searchMovie(query));
     }
   };
+
+  if(location.pathname === "/") return null;
   return (
     <div className={classes.searchContainer}>
       <TextField
