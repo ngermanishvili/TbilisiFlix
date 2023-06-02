087bb80921fba689769f04667ace64c20e4f0a22
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-03 02:57:44 +0400

    Import all neccesary styles 💅

--- src/index.js
+++ src/index.js
@@ -5,19 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import ToggleColorMode from './utils/ToggleColorMode';
 import './index.css'
 
-const theme = createTheme({
-  typography: {
-    fontSize: 16,
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorMode >
       <App />
-    </ThemeProvider>
+    </ToggleColorMode>
   </Provider>
 );
