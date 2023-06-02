aead8fdccf8a5e0848c3e30ff57948e54429b229
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-06-02 20:50:12 +0400

    Implemented Actors component with details and movie list

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,17 +1,102 @@
-import React from "react";
 import { useState } from "react";
 import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
-import { useHistory, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ArrowBack } from "@mui/icons-material";
+import MovieList from "../MovieList/MovieList";
 
-import { useGetActorsDetailsQuery } from "../../services/TMDB";
+import useStyles from "./styles";
+import {
+  useGetActorsDetailsQuery,
+  useGetMoviesByActorIdQuery,
+} from "../../services/TMDB";
 
 const Actors = () => {
-  const {id} = useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const classes = useStyles();
+  const page = 1;
+  const { data, isFetching, error } = useGetActorsDetailsQuery(id);
+  const { data: movies } = useGetMoviesByActorIdQuery({ id, page });
 
-  const {data, isFetching, error} = useGetActorsDetailsQuery(id);
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
 
-  return <div>Actor- {id} </div>;
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Button
+          startIcon={<ArrowBack />}
+          onClick={() => navigate("back")}
+          color="primary"
+        >
+          Go back to previous page
+        </Button>
+      </Box>
+    );
+  }
+
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item lg={5} xl={4}>
+          <img
+            className={classes.image}
+            src={`https://image.tmdb.org/t/p/w500/${data?.profile_path}`}
+            alt={data?.name}
+          />
+        </Grid>
+
+        <Grid
+          item
+          lg={7}
+          xl={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h2" gutterBottom>
+            {data?.name}
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            {data?.birthday} ({data?.place_of_birth})
+          </Typography>
+          <Typography variant="body2" align="justify" paragraph>
+            {data?.biography || "No biography available."}
+          </Typography>
+          <Box marginTop="2rem" display="flex" justifyContent="space-around">
+            <Button
+              variant="contained"
+              color="primary"
+              target="_blank"
+              href={`https://www.imdb.com/name/${data?.imdb_id}`}
+            >
+              IMDB
+            </Button>
+            <Button
+              startIcon={<ArrowBack />}
+              onClick={() => navigate("/")}
+              color="primary"
+            >
+              Go back to previous page
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+      <Box margin="2rem 0">
+        <Typography variant="h2" gutterBottom align="center">
+          Movies
+        </Typography>
+        {movies && <MovieList movies={movies} />},
+      </Box>
+    </>
+  );
 };
 
 export default Actors;
